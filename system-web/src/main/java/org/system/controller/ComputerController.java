12b4ddeea97a4c3f9b2d5e90c51d9239169ef4fd
liang.rx.hong@gmail.com <liang.rx.hong@gmail.com>
2016-06-13 10:13:25 +0800

    rebuild angular service

--- system-web/src/main/java/org/system/controller/ComputerController.java
+++ system-web/src/main/java/org/system/controller/ComputerController.java
@@ -1,9 +1,6 @@
 package org.system.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- system-web/src/main/webapp/app/app.js
+++ system-web/src/main/webapp/app/app.js
@@ -1,25 +1,12 @@
-var permissionList;
 angular.module("link",["ngRoute","link.controllers","link.services","link.permission","chieffancypants.loadingBar","ngAnimate"]).config(["$routeProvider","$locationProvider","$httpProvider",function($routeProvider,$locationProvider,$httpProvider,$scope){
-	$routeProvider.when("/home",{templateUrl:"templates/home.html",controller:"homeCtl",permission:"login"});
 	$routeProvider.when("/computer",{templateUrl:"templates/computer.html",controller:"computerCtl",permission:"login"});
-	$routeProvider.when("/linkroute",{templateUrl:"templates/linkroute.html",controller:"linkRouteCtl",permission:"login"});
-	$routeProvider.when("/user",{templateUrl:"templates/user.html",controller:"userCtl",permission:"admin"});
-	$routeProvider.when("/login",{templateUrl:"templates/login.html",controller:"userCtl",permission:""});
-	$routeProvider.when("/unAuth",{templateUrl:"templates/unAuth.html",permission:""});
-	$routeProvider.otherwise({redirectTo:"/home"});
+	$routeProvider.otherwise({redirectTo:"/computer"});
 }])
 .config(function(cfpLoadingBarProvider) {
     cfpLoadingBarProvider.includeSpinner = false;
 })
 
-.run(function(permissionService){
-	console.log("run",permissionList)
-	permissionService.setPermission(permissionList);
-})
 
 angular.element(document).ready(function(){
-	$.get("user/getPermission.do",function(data){
-		permissionList = data;
-		angular.bootstrap(document.body,['link']);
-	})
+	angular.bootstrap(document.body,['link']);
 })
